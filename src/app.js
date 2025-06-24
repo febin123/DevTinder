ae81b1d06afe33c3fd0a2c75ab2d426f605f12d7
febin123 <febsep96@gmail.com>
2025-06-24 20:38:00 +0530

    added GET api

--- src/app.js
+++ src/app.js
@@ -18,6 +18,28 @@ app.post('/signup', async(req, res) => {
    
 });
 
+//Find user by email-GET /user/:email
+app.get('/user',async(req,res)=>{
+    const userEmail=req.body.email;
+    try{
+        console.log("Fetching user with email:", userEmail);
+        const user=await User.findOne({email:userEmail})
+        if(!user){
+            console.log("User not found");
+            return res.status(404).send("User not found");
+        }
+        res.send(user);
+    }
+    catch(err){
+        res.status(400).send("Internal Server Error");
+      
+    }
+    
+})
+//Feed API-GET /feed get all users from the database
+app.get('/feed', async(req, res) => {
+
+})
 
 
 connectDB().then(()=>{
